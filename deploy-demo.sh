b03ef915aaaafb5850d42f726451baa5fcaf40c5
Ryan Heap <rheap@degdigital.com>
2016-08-16 15:03:08 -0500

    updated travis demo deploy script

--- deploy-demo.sh
+++ deploy-demo.sh
@@ -9,6 +9,8 @@ function buildDemo {
 	rsync -av --exclude='js/src' demo/ out
 }
 
+rm -rf out || exit 0;
+
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
 if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
     echo "Skipping deploy; just doing a build."
@@ -23,30 +25,23 @@ SHA=`git rev-parse --verify HEAD`
 
 # Clone the existing gh-pages for this repo into out/
 # Create a new empty branch if gh-pages doesn't exist yet (should only happen on first deply)
-git clone $REPO out
-cd out
-git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
-cd ..
+mkdir out
 
-# Clean out existing contents
-rm -rf out/**/* || exit 0
-
-# Run our compile script
 buildDemo
 
-# Now let's go have some fun with the cloned repo
 cd out
+git init
 git config user.name "Travis CI"
 git config user.email "$COMMIT_AUTHOR_EMAIL"
 
+
 # If there are no changes to the compiled out (e.g. this is a README update) then just bail.
-if [ -z `git diff --exit-code` ]; then
+if [ -z 'git diff --exit-code' ]; then
     echo "No changes to the output on this push; exiting."
     exit 0
 fi
 
-# Commit the "changes", i.e. the new version.
-# The delta will show diffs between new and old versions.
+# Add and commit changes
 git add .
 git commit -m "Deploy to GitHub Pages: ${SHA}"
 
@@ -61,4 +56,4 @@ eval `ssh-agent -s`
 ssh-add deploy_key
 
 # Now that we're all set up, we can push.
-git push $SSH_REPO $TARGET_BRANCH
+git push --force --quiet $SSH_REPO $SOURCE_BRANCH:$TARGET_BRANCH > /dev/null 2>&1
